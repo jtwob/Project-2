e166b1c8887b53d1dfb7211cef73869a9886ef50
jtwob <32282322+jtwob@users.noreply.github.com>
2021-01-21 20:13:09 -0800

    UPDATE: Added comments to user routes

--- routes/user-api-routes.js
+++ routes/user-api-routes.js
@@ -1,10 +1,17 @@
 var db = require("../models");
 
 module.exports = function (app) {
+  /**
+   * This is the get all route, using findAll() with no object passed.
+   */
   app.get("/api/user", (req, res) => {
-    db.User.findAll().then((dbUser) => res.json(dbUser));
+    db.User.findAll({}).then((dbUser) => res.json(dbUser));
   });
 
+  /**
+   * This is the route to find a specific user.
+   * The route is simply used like "/api/user/x" where x is the user id.
+   */
   app.get("/api/user/:id", (req, res) => {
     db.User.findOne({
       where: {
@@ -13,10 +20,20 @@ module.exports = function (app) {
     }).then((dbUser) => res.json(dbUser));
   });
 
+  /**
+   * This is the route for creating a new user.
+   * The req.body will require:
+   *    name, email, password
+   */
   app.post("/api/user", (req, res) => {
     db.User.create(req.body).then((dbUser) => res.json(dbUser));
   });
 
+  /**
+   * This is the route to update user info.
+   * The req.body will require:
+   *    id, + any updated fields
+   */
   app.put("/api/user", (req, res) => {
     db.User.update(req.body, {
       where: {
@@ -25,6 +42,10 @@ module.exports = function (app) {
     }).then((dbUser) => res.json(dbUser));
   });
 
+  /**
+   * This is the route to delete users.
+   * The route is simply used like "/api/user/x" where x is the user id.
+   */
   app.delete("/api/user/:id", (req, res) => {
     db.User.destroy({
       where: {
