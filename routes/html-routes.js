13e03b699b6e8bdb6a048ba51f16f4b49a791996
Nhi Danis <a.nhi1001@gmail.com>
2021-01-22 12:33:57 -0800

    add member front page

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 // Requiring path to so we can use relative routes to our HTML files
 var path = require("path");
+var db = require("../models");
 
 // Requiring our custom middleware for checking if a user is logged in
 var isAuthenticated = require("../config/middleware/isAuthenticated");
@@ -25,7 +26,24 @@ module.exports = function(app) {
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
   app.get("/members", isAuthenticated, function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/members.html"));
+    //res.sendFile(path.join(__dirname, "../public/members.html"));
+    // db.Connection.findAll({
+    //   include: [db.User],
+    // }).then((data) => {
+    console.log(`GETTING ALL CONNECTION`);
+    db.Connection.findAll()
+      .then((data) => {
+      console.log(` `);
+      console.log(` `);
+      console.log(`DATA RETURNED: ${data}`);
+      console.log(` `);
+      console.log(` `);
+
+      const allConnections = {
+            connections: data
+      };
+      res.render("index", allConnections);
+    });
   });
 
 };
